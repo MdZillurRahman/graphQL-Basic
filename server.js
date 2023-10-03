0b247fd4b32dedb8406effd3a1b36e3cc71383bc
MdZillurRahman <mzisanr@gmail.com>
2023-10-04 00:27:17 +0600

    Created Mutation to Add Book + Author

--- server.js
+++ server.js
@@ -111,10 +111,54 @@ const rootQueryType = new GraphQLObjectType({
   }),
 });
 
-
+const rootMutationType = new GraphQLObjectType({
+    name: "Mutation",
+    description: "Root Mutation",
+    fields: () => ({
+        addBook:{
+            type: BookType,
+            description:"Add a new book",
+            args:{
+                name:{
+                    type: GraphQLNonNull(GraphQLString)
+                },
+                authorId:{
+                    type: GraphQLNonNull(GraphQLInt)
+                }
+            },
+            resolve: (parent,args) => {
+                const book = {
+                    id: books.length + 1,
+                    name: args.name,
+                    authorId: args.authorId
+                }
+                books.push(book);
+                return book;
+            }
+        },
+        addAuthor:{
+            type: AuthorType,
+            description:"Add a new author",
+            args:{
+                name:{
+                    type: GraphQLNonNull(GraphQLString)
+                }
+            },
+            resolve: (parent,args) => {
+                const author = {
+                    id: authors.length + 1,
+                    name: args.name
+                }
+                authors.push(author);
+                return author;
+            }
+        },
+    })
+});
 
 const schema = new GraphQLSchema({
   query: rootQueryType,
+  mutation: rootMutationType,
 });
 
 app.use(
